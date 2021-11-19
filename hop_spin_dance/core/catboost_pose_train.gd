a62ed7a789924c80ef39dfbb2aad2b7cd8836eda
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-19 09:52:34 -0800

    Generalize.

--- hop_spin_dance/core/catboost_pose_train.gd
+++ hop_spin_dance/core/catboost_pose_train.gd
@@ -12,16 +12,6 @@ func _write_test(scene):
 	while not queue.is_empty():
 		var front = queue.front()
 		var node = front
-#		if node is AnimationPlayer:
-#			var anims = node.get_animation_list()
-#			for anim_i in anims:
-#				var animation = node.get_animation(anim_i)
-#				for track_i in animation.get_track_count():
-#					var path : String = animation.track_get_path(track_i)
-#					var splits = path.split("/", true, 1)
-#					if splits.size() > 1:
-#						path = path.split("/", true, 1)[1]
-#						animation.track_set_path(track_i, path)
 		if node is Skeleton3D:
 			var skeleton : Skeleton3D = node
 			var title : String
@@ -165,9 +155,6 @@ func _write_test(scene):
 				for bone in skel:
 					current_file.store_csv_line(bone.values(), "\t")
 			current_file.close()
-
-			
-			break
 		var child_count : int = node.get_child_count()
 		for i in child_count:
 			queue.push_back(node.get_child(i))
