651abc1a0535d0e76f5378de5088be016a7c2220
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-20 10:55:22 -0800

    Update to latest.

--- addons/catboost/import.gd
+++ addons/catboost/import.gd
@@ -1,27 +1,26 @@
 @tool
 extends EditorScenePostImport
 
+
 func _post_import(scene):
 	_write_test(scene)
 	return scene
-	
+
 var catboost = load("res://addons/catboost/catboost.gd")
 
-func _write_test(scene):
+func _write_test(scene):	
 	var file = File.new()
-	file.open(catboost.train_path, File.WRITE)
-	file.close()
-	var train : String = file.get_as_text()
 	file.open(catboost.train_description_path, File.WRITE)
 	var init_dict = catboost.bone_create()
-	var description = init_dict.description
-	file.store_string(description)
+	var description : PackedStringArray = init_dict.description
+	var file_string : String
+	for string in description:
+		file_string += string + "\n"
+	file.store_string(file_string)
 	var file_description = File.new()
 	file.open(catboost.train_path, File.WRITE)
-	file.store_string(train)
 	var queue : Array # Node
 	queue.push_back(scene)
-	var skeletons : Dictionary
 	while not queue.is_empty():
 		var front = queue.front()
 		var node = front
@@ -51,7 +50,7 @@ func _write_test(scene):
 							var author : String
 							var columns_description : PackedStringArray
 							var first : bool = true
-							var bone : Dictionary = catboost.bone_create("res://column_description.txt")
+							var bone : Dictionary = catboost.bone_create().bone
 							var bone_pose = skeleton.get_bone_global_pose(bone_i)
 							bone["Bone X global location in meters"] = bone_pose.origin.x
 							bone["Bone Y global location in meters"] = bone_pose.origin.y
@@ -85,25 +84,80 @@ func _write_test(scene):
 								bone["Bone Parent Y global scale in meters"] = parent_scale.y
 								bone["Bone Parent Z global scale in meters"] = parent_scale.z
 							bone["Animation Time"] = float(count_i) / fps
-							bone["Title"] = ""
-							bone["Author"] = ""
-							bone["Bone Name"] = bone_name
-							
-							var bone_parent_key = "Bone Parent"
+							bone["BONE"] = bone_name							
+							var bone_parent_key = "BONE_PARENT"
 							var parent_bone = skeleton.get_bone_name(bone_parent)
 							if not parent_bone.is_empty():
 								bone[bone_parent_key] = parent_bone
-							var vrm_bone_name_key = "Corresponding VRM Bone"
+							var vrm_bone_name_key = "VRM_BONE"
 							if catboost.vrm_humanoid_bones.has(skeleton.get_bone_name(bone_i)):
 								bone[vrm_bone_name_key] = skeleton.get_bone_name(bone_i)
-							bone["Animation"] = animation.resource_name
-							var header : Array
+							bone["ANIMATION"] = animation.resource_name
 							file.store_csv_line(bone.values(), "\t")
-			break
+		elif node is Skeleton3D:
+			var skeleton : Skeleton3D = node
+			var vrm_extension = scene
+			var title : String = vrm_extension["vrm_meta"].get("title")
+			var author : String = vrm_extension["vrm_meta"].get("author")
+			var skel : Array
+			skel.resize(skeleton.get_bone_count())
+			for vrm_def_bone_name in catboost.vrm_humanoid_bones:
+				for bone_i in skeleton.get_bone_count():
+					var bone : Dictionary = catboost.bone_create()
+					var bone_pose = skeleton.get_bone_global_pose(bone_i)
+					bone["Bone X global location in meters"] = bone_pose.origin.x
+					bone["Bone Y global location in meters"] = bone_pose.origin.y
+					bone["Bone Z global location in meters"] = bone_pose.origin.z
+					var basis = bone_pose.basis.orthonormalized()
+					bone["Bone truncated normalized basis axis x 0"] = basis.x.x
+					bone["Bone truncated normalized basis axis x 1"] = basis.x.y
+					bone["Bone truncated normalized basis axis x 2"] = basis.x.z
+					bone["Bone truncated normalized basis axis y 0"] = basis.y.x
+					bone["Bone truncated normalized basis axis y 1"] = basis.y.y
+					bone["Bone truncated normalized basis axis y 2"] = basis.y.z
+					var scale = bone_pose.basis.get_scale()
+					bone["Bone X global scale in meters"] = scale.x
+					bone["Bone Y global scale in meters"] = scale.y
+					bone["Bone Z global scale in meters"] = scale.z
+					var bone_parent = skeleton.get_bone_parent(bone_i)
+					if bone_parent != -1:
+						var bone_parent_pose = skeleton.get_bone_global_pose(bone_parent)
+						bone["Bone parent X global location in meters"] = bone_pose.origin.x
+						bone["Bone parent Y global location in meters"] = bone_pose.origin.y
+						bone["Bone parent Z global location in meters"] = bone_pose.origin.z
+						var parent_basis = bone_parent_pose.basis.orthonormalized()
+						bone["Bone parent truncated normalized basis axis x 0"] = parent_basis.x.x
+						bone["Bone parent truncated normalized basis axis x 1"] = parent_basis.x.y
+						bone["Bone parent truncated normalized basis axis x 2"] = parent_basis.x.z
+						bone["Bone parent truncated normalized basis axis y 0"] = parent_basis.y.x
+						bone["Bone parent truncated normalized basis axis y 1"] = parent_basis.y.y
+						bone["Bone parent truncated normalized basis axis y 2"] = parent_basis.y.z
+						var parent_scale = bone_parent_pose.basis.get_scale()
+						bone["Bone parent X global scale in meters"] = parent_scale.x
+						bone["Bone parent Y global scale in meters"] = parent_scale.y
+						bone["Bone parent Z global scale in meters"] = parent_scale.z
+					bone["BONE"] = skeleton.get_bone_name(bone_i)
+					var parent_bone = skeleton.get_bone_name(bone_parent)
+					if not parent_bone.is_empty():
+						bone[ "BONE_PARENT"] = parent_bone
+					bone["VRM_BONE"] = vrm_def_bone_name
+					bone["Label"] = 0
+					var version = vrm_extension["vrm_meta"].get("specVersion")
+					if version == null:
+						version = ""
+					bone["SPECIFICATION_VERSION"] = version
+					bone["ANIMATION"] = "T-Pose"
+					skel[bone_i] = bone
+				for humanBoneName in vrm_extension["vrm_meta"]["humanoid_bone_mapping"]:
+					var bone_name = vrm_extension["vrm_meta"]["humanoid_bone_mapping"][humanBoneName]
+					var bone_id = skeleton.find_bone(bone_name)
+					if bone_id != -1:
+						skel[bone_id]["VRM_BONE"] = humanBoneName
+						skel[bone_id]["Label"] = 1
+				for bone in skel:
+					file.store_csv_line(bone.values(), "\t")
 		var child_count : int = node.get_child_count()
 		for i in child_count:
 			queue.push_back(node.get_child(i))
-		queue.pop_front()		
-
+		queue.pop_front()
 	file.close()
-	return scene
--- addons/catboost/training_inference.md
+++ addons/catboost/training_inference.md
@@ -2,6 +2,6 @@ https://github.com/catboost/catboost/releases/tag/v1.0.3
 
 catboost fit --learn-set train.tsv --test-set test.tsv --column-description train_description.txt --custom-loss="AUC,Precision,Recall" --logging-level Verbose --loss-function Logloss --text-processing processing.json 
 
-catboost calc -m model.bin --column-description test_description.txt -o custom_data.eval -T 4 --output-columns TotalUncertainty,#29:VRM\ Bone,#27:Bone,Class --input-path test.tsv  --output-path output.tsv
+catboost calc -m model.bin --column-description test_description.txt -T 4 --output-columns "TotalUncertainty,#5:VRM Bone,#3:Bone,Class" --input-path test.tsv  --output-path output.tsv
 
-Open in openoffice calc and sort by class desc, uncertain #1, uncertain #2, vrm bone name, bone name
+Open in libreoffice calc and sort by class desc, uncertain #1, uncertain #2, bone name, vrm bone name
