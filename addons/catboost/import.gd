aa0bb27f52155b7cc39cd282044635b1fd20e5bf
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-21 15:52:39 -0800

    Don't duplicate as much.

--- addons/catboost/import.gd
+++ addons/catboost/import.gd
@@ -154,71 +154,70 @@ func _write_test(scene):
 						file.store_csv_line(bone.values(), "\t")
 		elif node is Skeleton3D:
 			var skeleton : Skeleton3D = node
-			for vrm_def_bone_name in catboost.vrm_humanoid_bones:
-				for bone_i in skeleton.get_bone_count():
-					var bone : Dictionary = catboost.bone_create().bone
-					var bone_rest = skeleton.get_bone_rest(bone_i)
-					bone["Bone rest X global origin in meters"] = bone_rest.origin.x
-					bone["Bone rest Y global origin in meters"] = bone_rest.origin.x
-					bone["Bone rest Z global origin in meters"] = bone_rest.origin.x
-					var bone_rest_basis = bone_rest.basis.orthonormalized()
-					bone["Bone rest truncated normalized basis axis x 0"] = bone_rest_basis.x.x
-					bone["Bone rest truncated normalized basis axis x 1"] = bone_rest_basis.x.y
-					bone["Bone rest truncated normalized basis axis x 2"] = bone_rest_basis.x.z
-					bone["Bone rest truncated normalized basis axis y 0"] = bone_rest_basis.y.x
-					bone["Bone rest truncated normalized basis axis y 1"] = bone_rest_basis.y.y
-					bone["Bone rest truncated normalized basis axis y 2"] = bone_rest_basis.y.z
-					var bone_rest_scale = bone_rest.basis.get_scale()	
-					bone["Bone rest X global scale in meters"] = bone_rest_scale.x
-					bone["Bone rest Y global scale in meters"] = bone_rest_scale.y
-					bone["Bone rest Z global scale in meters"] = bone_rest_scale.z
-					var bone_pose = skeleton.get_bone_global_pose(bone_i)
-					bone["Bone X global origin in meters"] = bone_pose.origin.x
-					bone["Bone Y global origin in meters"] = bone_pose.origin.y
-					bone["Bone Z global origin in meters"] = bone_pose.origin.z
-					var basis = bone_pose.basis.orthonormalized()
-					bone["Bone truncated normalized basis axis x 0"] = basis.x.x
-					bone["Bone truncated normalized basis axis x 1"] = basis.x.y
-					bone["Bone truncated normalized basis axis x 2"] = basis.x.z
-					bone["Bone truncated normalized basis axis y 0"] = basis.y.x
-					bone["Bone truncated normalized basis axis y 1"] = basis.y.y
-					bone["Bone truncated normalized basis axis y 2"] = basis.y.z
-					var scale = bone_pose.basis.get_scale()
-					bone["Bone X global scale in meters"] = scale.x
-					bone["Bone Y global scale in meters"] = scale.y
-					bone["Bone Z global scale in meters"] = scale.z
-					var bone_parent = skeleton.get_bone_parent(bone_i)
-					if bone_parent != -1:
-						var bone_parent_pose = skeleton.get_bone_global_pose(bone_parent)
-						bone["Bone parent X global origin in meters"] = bone_pose.origin.x
-						bone["Bone parent Y global origin in meters"] = bone_pose.origin.y
-						bone["Bone parent Z global origin in meters"] = bone_pose.origin.z
-						var parent_basis = bone_parent_pose.basis.orthonormalized()
-						bone["Bone parent truncated normalized basis axis x 0"] = parent_basis.x.x
-						bone["Bone parent truncated normalized basis axis x 1"] = parent_basis.x.y
-						bone["Bone parent truncated normalized basis axis x 2"] = parent_basis.x.z
-						bone["Bone parent truncated normalized basis axis y 0"] = parent_basis.y.x
-						bone["Bone parent truncated normalized basis axis y 1"] = parent_basis.y.y
-						bone["Bone parent truncated normalized basis axis y 2"] = parent_basis.y.z
-						var parent_scale = bone_parent_pose.basis.get_scale()
-						bone["Bone parent X global scale in meters"] = parent_scale.x
-						bone["Bone parent Y global scale in meters"] = parent_scale.y
-						bone["Bone parent Z global scale in meters"] = parent_scale.z
-					if bone["BONE"].is_empty():
-						continue
-					bone["BONE"] = skeleton.get_bone_name(bone_i)
-					var hierachy : String = catboost.print_skeleton_neighbours_text(print_skeleton_neighbours_text_cache, skeleton, bone["BONE"])
-					if not hierachy.is_empty():
-						bone["BONE_HIERARCHY"] = hierachy
-					bone["BONE_CAPITALIZED"] = bone["BONE"].capitalize()
-					var parent_bone = skeleton.get_bone_name(bone_parent)
-					if bone_map.has(bone["BONE"]):
-						bone["Label"] = bone_map[bone["BONE"]]
-					var version = vrm_extension["vrm_meta"].get("specVersion")
-					if version == null or version.is_empty():
-						version = "1.0"
-					bone["SPECIFICATION_VERSION"] = version
-					file.store_csv_line(bone.values(), "\t")
+			for bone_i in skeleton.get_bone_count():
+				var bone : Dictionary = catboost.bone_create().bone
+				var bone_rest = skeleton.get_bone_rest(bone_i)
+				bone["Bone rest X global origin in meters"] = bone_rest.origin.x
+				bone["Bone rest Y global origin in meters"] = bone_rest.origin.x
+				bone["Bone rest Z global origin in meters"] = bone_rest.origin.x
+				var bone_rest_basis = bone_rest.basis.orthonormalized()
+				bone["Bone rest truncated normalized basis axis x 0"] = bone_rest_basis.x.x
+				bone["Bone rest truncated normalized basis axis x 1"] = bone_rest_basis.x.y
+				bone["Bone rest truncated normalized basis axis x 2"] = bone_rest_basis.x.z
+				bone["Bone rest truncated normalized basis axis y 0"] = bone_rest_basis.y.x
+				bone["Bone rest truncated normalized basis axis y 1"] = bone_rest_basis.y.y
+				bone["Bone rest truncated normalized basis axis y 2"] = bone_rest_basis.y.z
+				var bone_rest_scale = bone_rest.basis.get_scale()	
+				bone["Bone rest X global scale in meters"] = bone_rest_scale.x
+				bone["Bone rest Y global scale in meters"] = bone_rest_scale.y
+				bone["Bone rest Z global scale in meters"] = bone_rest_scale.z
+				var bone_pose = skeleton.get_bone_global_pose(bone_i)
+				bone["Bone X global origin in meters"] = bone_pose.origin.x
+				bone["Bone Y global origin in meters"] = bone_pose.origin.y
+				bone["Bone Z global origin in meters"] = bone_pose.origin.z
+				var basis = bone_pose.basis.orthonormalized()
+				bone["Bone truncated normalized basis axis x 0"] = basis.x.x
+				bone["Bone truncated normalized basis axis x 1"] = basis.x.y
+				bone["Bone truncated normalized basis axis x 2"] = basis.x.z
+				bone["Bone truncated normalized basis axis y 0"] = basis.y.x
+				bone["Bone truncated normalized basis axis y 1"] = basis.y.y
+				bone["Bone truncated normalized basis axis y 2"] = basis.y.z
+				var scale = bone_pose.basis.get_scale()
+				bone["Bone X global scale in meters"] = scale.x
+				bone["Bone Y global scale in meters"] = scale.y
+				bone["Bone Z global scale in meters"] = scale.z
+				var bone_parent = skeleton.get_bone_parent(bone_i)
+				if bone_parent != -1:
+					var bone_parent_pose = skeleton.get_bone_global_pose(bone_parent)
+					bone["Bone parent X global origin in meters"] = bone_pose.origin.x
+					bone["Bone parent Y global origin in meters"] = bone_pose.origin.y
+					bone["Bone parent Z global origin in meters"] = bone_pose.origin.z
+					var parent_basis = bone_parent_pose.basis.orthonormalized()
+					bone["Bone parent truncated normalized basis axis x 0"] = parent_basis.x.x
+					bone["Bone parent truncated normalized basis axis x 1"] = parent_basis.x.y
+					bone["Bone parent truncated normalized basis axis x 2"] = parent_basis.x.z
+					bone["Bone parent truncated normalized basis axis y 0"] = parent_basis.y.x
+					bone["Bone parent truncated normalized basis axis y 1"] = parent_basis.y.y
+					bone["Bone parent truncated normalized basis axis y 2"] = parent_basis.y.z
+					var parent_scale = bone_parent_pose.basis.get_scale()
+					bone["Bone parent X global scale in meters"] = parent_scale.x
+					bone["Bone parent Y global scale in meters"] = parent_scale.y
+					bone["Bone parent Z global scale in meters"] = parent_scale.z
+				if bone["BONE"].is_empty():
+					continue
+				bone["BONE"] = skeleton.get_bone_name(bone_i)
+				var hierachy : String = catboost.print_skeleton_neighbours_text(print_skeleton_neighbours_text_cache, skeleton, bone["BONE"])
+				if not hierachy.is_empty():
+					bone["BONE_HIERARCHY"] = hierachy
+				bone["BONE_CAPITALIZED"] = bone["BONE"].capitalize()
+				var parent_bone = skeleton.get_bone_name(bone_parent)
+				if bone_map.has(bone["BONE"]):
+					bone["Label"] = bone_map[bone["BONE"]]
+				var version = vrm_extension["vrm_meta"].get("specVersion")
+				if version == null or version.is_empty():
+					version = "1.0"
+				bone["SPECIFICATION_VERSION"] = version
+				file.store_csv_line(bone.values(), "\t")
 		var child_count : int = node.get_child_count()
 		for i in child_count:
 			queue.push_back(node.get_child(i))
